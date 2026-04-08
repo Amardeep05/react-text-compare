a212ceab02937ed9cf08ab234fe1fa75ca1aa631
Amardeep Singh <amardeepdharwan@gmail.com>
2026-04-09 00:58:21 +0530

    Fixed eslint build errors for Azure deployment

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <TextCompare/>
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 );
 
